c8d8952f2582dd6d555d608e8c52d432fe05ad68
ritik dwivedi <86961733+ritik1172@users.noreply.github.com>
2023-02-10 18:46:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// clear, equate, display
+
+function clr(){
+    document.getElementById("result").value = "";
+}
+
+function display(val){
+    document.getElementById("result").value+= val; // val = val + val
+
+}
+
+function equate(){
+     let x = document.getElementById("result").value;
+     let y = eval(x);
+     document.getElementById("result").value = y;
+}
